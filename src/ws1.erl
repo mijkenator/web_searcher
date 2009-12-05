1f2ce391ad065dab30e314bdbe2211a3e7c7add0
root <root@localhost.localdomain>
2009-12-05 18:23:17 +0300

    fixs

--- src/ws1.erl
+++ src/ws1.erl
@@ -4,71 +4,78 @@
 -include_lib("stdlib/include/qlc.hrl").
 -include_lib("jobrec.hrl").
 -export([start/0, worker/3, get_urls/3, trim_slash/1, trim_last_slash/1,
-        save_url/1, check_not_exists/1, get_job/0, msleep/1, prepare1/0]).
+        save_url/1, check_not_exists/1, get_job/0, msleep/1, prepare1/0, start/2, starter/2]).
 
 start() ->
-    lists:foreach(fun(X) -> spawn(ws1, worker, [<<"http://www.perl.com">>, X, 0]) end, lists:seq(1,50)).
+    lists:foreach(fun(X) -> Pid = spawn(ws1, worker, [<<"http://www.perl.com">>, X, 0]),
+    io:format("Pid -> ~p ~n", [Pid]), register(toProf, Pid) end, lists:seq(1,1)).
 
-worker(U, Number, Counter) when Counter < 10 ->
+start(Url, CountWorker) ->
+    Pid = spawn(ws1, starter, [Url, CountWorker]),
+    register(ws1Prof, Pid),
+    Pid.
+
+starter(Url, CountWorker) ->
+    spawn(ws1, worker, [Url, 0, 0]),
+    msleep(20),
+    lists:foreach(fun(X) -> spawn(ws1, worker, [<<>>, X, 0]) end, lists:seq(1, CountWorker)).
+
+worker(U, Number, Counter) when U =:= <<>> ->
+    case mnesia:transaction(fun() -> get_job() end) of
+        {atomic, {ok, NewUrl}} ->
+            io:format("get job OK -> ~p ~n", [NewUrl]),
+            worker(NewUrl, Number, Counter+1);
+        _R   ->
+            io:format("Exit -> ~p ~n", [_R]),
+            true
+    end;
+worker(U, Number, Counter) when Counter < 200 ->
     Url = binary_to_list(U),
-    io:format("Worker number ~p with ~p called -> ~n", [Number, Url]),
+    io:format("Worker number ~p (~p) with ~p called -> ~n", [Number, Counter, Url]),
     case http:request(get, {Url, []}, [{timeout, 20000}, {autoredirect, true}], [{body_format, binary}]) of
         {ok, {_Status, _Headers, Body}} ->
             io:format("job ~p body length -> ~p bytes ~n", [Url, size(Body)]),
-            case get_urls(Body, Url, binregex) of
-                {ok, _M} ->
-                    case mnesia:transaction(fun() -> get_job() end) of
-                        {atomic, {ok, NewUrl}} -> io:format("get job OK -> ~p ~n", [NewUrl]), worker(NewUrl, Number, Counter+1);
-                        _R   -> true
-                    end;
-                {error, _M} -> true
-            end;
+            get_urls(Body, Url, binregex);
         {error, Reason} ->
-            io:format("job ~p failed -> ~p ~n", [Url, Reason]),
-            case mnesia:transaction(fun() -> get_job() end) of
-                {atomic, {ok, NewUrl}} -> io:format("get job OK -> ~p ~n", [NewUrl]), worker(NewUrl, Number, Counter+1);
-                _R   -> true
-            end
-    end;
-worker(_U, _Number, _Counter) -> true.
+            io:format("job ~p failed -> ~p ~n", [Url, Reason])
+    end,
+    case mnesia:transaction(fun() -> get_job() end) of
+        {atomic, {ok, NewUrl}} ->
+            io:format("get job OK -> ~p ~n", [NewUrl]),
+            worker(NewUrl, Number, Counter+1);
+        _R   ->
+            io:format("Exit -> ~p ~n", [_R]),
+            true
+    end;    
+worker(_U, _Number, _Counter) ->
+    io:format("Work down -> ~p ~n", [_Counter]),
+    true.
     
 get_urls(HtmlBinary, MainUrl, Type) when Type =:= binregex ->
     MainUrlBin = list_to_binary(MainUrl),
     MainUrl1 = trim_slash(MainUrlBin),
     Complement = fun(Url) ->
-        case re:run(Url, "^http(s)*://", [{capture,[1]}]) of
-            nomatch ->
+        case checkHttp(Url) of
+            false ->
                 Url1 = trim_slash(Url),
                 <<MainUrl1/binary,"/",Url1/binary>>;
             _ -> Url
         end
     end,
-    CheckUrl = fun(Url) ->
-        case re:run(Url, "\.(pdf|mp3|doc|tar|rar|zip|tgz|tar.gz|js|css|tar.bz2|bz2)$", [{capture,[1]}]) of
-            {match, _} -> false;
-            _          -> true
-        end
-    end,
-    GrepHttp = fun(Url) ->
-        case re:run(Url, "^http(s)*://", [{capture,[1]}]) of
-            {match, _} -> CheckUrl(Url);
-            _          -> false
-        end
-    end,
     ProcessUrl = fun([{Start, Length}]) ->
         <<_:Start/binary,Url:Length/binary,_/binary>> = HtmlBinary,
-        U1 = Complement(Url),
-        case GrepHttp(U1) of
-            true -> save_url(U1);
+        case checkUrl(Url) of
+            true -> save_url(Complement(clear_url(Url)));
             _    -> false
         end   
     end,
     Reg = "<a.*?href=['\"]*([^'\"\s]+)['\"]*.*?>",
     case re:run(HtmlBinary, Reg, [global, {capture,[1]}, {newline,any}]) of
-        {match, A} when is_list(A) -> lists:map(fun(Url) -> ProcessUrl(Url) end, A), {ok, done};
+        {match, A} when is_list(A) -> lists:foreach(fun(Url) -> ProcessUrl(Url) end, A), {ok, done};
         _   -> {error, get_url_error}
     end.
 
+trim_slash(Str) when is_binary(Str), Str =:= <<>> -> <<>>;
 trim_slash(Str) when is_binary(Str), Str =:= <<"/">> -> <<>>;
 trim_slash(Str) when is_binary(Str) ->
     <<A:1/binary, B/binary>> = Str,
@@ -91,30 +98,60 @@ trim_last_slash(Bin) ->
 
 save_url(Url) ->
     case check_not_exists(Url) of
-        true ->     Row = #jobrec{url=Url, state=new},
-                    F = fun() -> mnesia:write(Row) end,
-                    mnesia:transaction(F), true;
+        true ->     mnesia:transaction(fun() -> mnesia:write(#jobrec{url=Url, state=new}) end), true;
         _    ->     false
     end.
 
 check_not_exists(Url) ->
-    Ans = mnesia:transaction(fun() -> mnesia:select(jobrec, [{#jobrec{state='$1', url=Url}, [], ['$1']}], 1, read) end ),
-    case Ans of
-        {atomic, {[], _}}-> true;
-        {atomic, {_, _}} -> false;
-        _                -> true
+    case mnesia:dirty_read({jobrec, Url}) of
+        []    -> true;
+        _     -> false
     end.
+
     
 get_job() ->
     Ans = mnesia:select(jobrec, [{#jobrec{state=new, url='$1'}, [], ['$1']}], 1, write),
     case Ans of
         {[Url|_], _} -> mnesia:write(#jobrec{url=Url, state=processing}), {ok, Url};
-        _                      ->
-            %io:format("GET JOB failed -> ~p ~n", [Ans]),
-            %{error, wait_for_job}
-            msleep(5),
-            get_job()
+        _                      -> msleep(5), get_job()
     end.
+
+clear_url(Url) ->
+    S = size(Url), S1 = S-1, S2 = S-2,
+    case Url of
+        <<R:S1/binary,"\r">> -> R;
+        <<R:S1/binary,"\n">> -> R;
+        <<R:S2/binary,"\n\r">> -> R;
+        <<R:S2/binary,"\r\n">> -> R;
+        _ -> Url
+    end.
+
+checkHttp(Url) ->
+    case Url of
+        <<"http://", _/binary>>  -> true;
+        <<"https://", _/binary>> -> true;
+        _ -> false
+    end.
+
+checkUrl(Url) ->
+    S = size(Url),
+    S4 = S - 4, S3 = S - 3,  
+    case Url of
+        <<_:S4/binary, ".pdf">>  -> false;
+        <<_:S4/binary, ".mp3">>  -> false;
+        <<_:S4/binary, ".doc">>  -> false;
+        <<_:S4/binary, ".rar">>  -> false;
+        <<_:S4/binary, ".tgz">>  -> false;
+        <<_:S3/binary, ".js">>   -> false;
+        <<_:S4/binary, ".css">>  -> false;
+        <<_:S4/binary, ".bz2">>  -> false;
+        <<_:S3/binary, ".gz">>   -> false;
+        <<"#", _/binary>>        -> false;
+        <<"/">>                  -> false;
+        <<"/#">>                 -> false;
+        _ -> true
+    end.
+
 
 msleep(T) when is_integer(T), T > 0 ->
     receive
